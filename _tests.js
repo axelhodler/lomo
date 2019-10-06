25de8197a2ecc14e4460665bf6314905d0659eb2
Axel Hodler <axel@hodler.co>
2019-10-06 12:35:42 +0200

    extract page object

--- _tests.js
+++ _tests.js
@@ -11,15 +11,27 @@ const clearCanvasContents = () => {
   document.getElementById("canvas").innerHTML = ""
 }
 
+class NotificationPageObject {
+  // obj should contain knowledge on bootstrap classes
+  _element = document.getElementById("notification")
+
+  hasClass(clazz) {
+    return this._element.getAttribute("class").includes(clazz)
+  }
+  hasInnerHTML(html) {
+    return this._element.innerHTML === html
+  }
+}
+
 QUnit.test("rendering success message", (assert) => {
   prepareCanvas()
   const message = "success"
 
   renderSuccessMessage(message)
 
-  const notification = document.getElementById("notification")
-  assert.ok(notification.getAttribute("class").includes("alert-success"))
-  assert.ok(notification.innerHTML === message)
+  const notification = new NotificationPageObject()
+  assert.ok(notification.hasClass("alert-success"))
+  assert.ok(notification.hasInnerHTML(message))
   clearCanvasContents()
 });
 
@@ -29,9 +41,8 @@ QUnit.test("rendering fail message", (assert) => {
 
   renderFailMessage(message)
 
-  const notification = document.getElementById("notification")
-
-  assert.ok(notification.getAttribute("class").includes("alert-danger"))
-  assert.ok(notification.innerHTML === message)
+  const notification = new NotificationPageObject()
+  assert.ok(notification.hasClass("alert-danger"))
+  assert.ok(notification.hasInnerHTML(message))
   clearCanvasContents()
 });
