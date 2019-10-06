8c5cb3bdeda83641d1f1d2d5cffb3ce90f1ce96b
Axel Hodler <axel@hodler.co>
2019-10-06 11:58:34 +0200

    extract creating and cleaning canvas

--- _tests.js
+++ _tests.js
@@ -1,13 +1,22 @@
-QUnit.test("rendering success message", (assert) => {
+const prepareCanvas = () => {
   const canvas = document.getElementById("canvas")
   const component = document.createElement("div")
   component.setAttribute("id", "info")
   const filler = document.createElement("div")
   component.appendChild(filler)
   canvas.appendChild(component)
+}
+
+const removeCanvas = () => {
+  document.getElementById("canvas").remove()
+}
+
+QUnit.test("rendering success message", (assert) => {
+  prepareCanvas()
   const message = "success"
 
   renderSuccessMessage(message)
 
   assert.ok(document.getElementById("notification").innerHTML === message)
+  removeCanvas()
 });
