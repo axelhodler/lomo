4e6f8f6d46f192e6ba8ccd10927810d1a240678c
Axel Hodler <axel.hodler@porsche.digital>
2019-10-16 15:02:23 +0200

    quick dax support

--- init.js
+++ init.js
@@ -13,7 +13,8 @@ const showDecisionButtons = () => {
 async function initGame() {
   const response = await fetch('https://d1p2el6716rm7u.cloudfront.net/');
   const marketCaps = await response.json()
-  carcompanies = marketCaps.car_companies
+  const params = new URL(window.location).searchParams;
+  carcompanies = marketCaps[params.get("type") ? params.get("type") : "car_companies"]
 
   document.getElementById("up").addEventListener("click", () => {
     const company = carcompanies[0]
